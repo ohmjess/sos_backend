f2bbfb57ed156464861465d864f61347380bfae1
Jessada.n <Jessada.n@clicknext.com>
2025-02-21 02:10:59 +0700

    Add dashboard route to server

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,11 @@ import { authRouter } from "./routes/auth.routes";
 import { projectRouter } from "./routes/project.routes";
 import { typeRouter } from "./routes/type.routes";
 import { serviceReportRouter } from "./routes/serviceReport.routes";
+import { dashboardRouter } from "./routes/dashboard.routes";
 
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/projects", projectRouter)
 app.use("/api/types", typeRouter)
 app.use("/api/serviceReport", serviceReportRouter)
+app.use("/api/dashboard", dashboardRouter)
