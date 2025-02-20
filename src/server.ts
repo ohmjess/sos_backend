bf898d1eccded0d878e5009fcd7126223c63b1f3
Jessada.n <Jessada.n@clicknext.com>
2025-02-20 21:13:23 +0700

    feat - Add service report router to API

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,10 @@ import { userRouter } from "./routes/user.routes"
 import { authRouter } from "./routes/auth.routes";
 import { projectRouter } from "./routes/project.routes";
 import { typeRouter } from "./routes/type.routes";
+import { serviceReportRouter } from "./routes/serviceReport.routes";
 
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/projects", projectRouter)
 app.use("/api/types", typeRouter)
+app.use("/api/serviceReport", serviceReportRouter)
